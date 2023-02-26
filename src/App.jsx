9b7c4b054a47e500091cb233c6db4badc1bf98b7
dluca22 <luc.due@tim.it>
2023-02-26 14:54:01 +0100

    styled navbar and menu for small screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './app.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-blue-200 ">
-      <h1>Helò</h1>
-    </div>
+   <div className='app'>
+    <Navbar />
+   </div>
   )
 }
 
