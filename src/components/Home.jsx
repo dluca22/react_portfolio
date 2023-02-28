6e6d45e9e75dc28040dda79b3da7066ea7df645f
dluca22 <luc.due@tim.it>
2023-02-28 17:24:54 +0100

    fixed position Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,11 @@ function Home() {
   return (
     //  TODO fix border and shadow
     //  border-b-8 border-[#98C1D9] shadow-xl shadow-[#98C1D9]
-    <div name='home' className='bg-[#293241] w-full h-screen md:h-[70vh] md:border-b-8 md:border-[#98c1d94b] md:shadow-[0_5px_60px_30px_#98c1d94b] '>
+    <div name='home' className='bg-[#293241] w-full h-screen md:h-[70vh] md:border-b-8 md:border-[#98c1d94b] md:shadow-[0_5px_60px_30px_#98c1d94b]
+    flex justify-center '>
       {/* container */}
       {/* ci può anche stare un breakpoint xs in cui fa in full e la foto viene spostata sopra */}
-      <div className=' w-full sm:w-2/3 lg:1/2  px-10 md:px-8 flex flex-col justify-end pb-10  h-full '>
+      <div className=' w-full sm:w-2/3 lg:w-1/2  px-10 md:px-8 flex flex-col justify-end pb-10  h-full '>
         <h1 className='text-4xl font-bold tracking-wide text-[#98C1D9] '>
           {' '}
           Luca De Angelis
